37497b08aa4c5ff3652339e1b67ffc733d576e12
Kirigaya21 <33805965+Kirigaya21@users.noreply.github.com>
2019-06-25 12:06:14 +0300

    version2

--- shopping-web-app/src/environments/environment.prod.ts
+++ shopping-web-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+        apiKey: "AIzaSyDMADBa0OhLvdfy2t_pnoyjgPExOvlYVq0",
+	    authDomain: "shopping-backend-firebas-d3183.firebaseapp.com",
+	    databaseURL: "https://shopping-backend-firebas-d3183.firebaseio.com",
+	    projectId: "shopping-backend-firebas-d3183",
+	    storageBucket: "shopping-backend-firebas-d3183.appspot.com",
+	    messagingSenderId: "1075460438294",
+	    appId: "1:1075460438294:web:658ac6abeb7109de"
+    }
 };
--- shopping-web-app/src/environments/environment.ts
+++ shopping-web-app/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase: {
+        apiKey: "AIzaSyDMADBa0OhLvdfy2t_pnoyjgPExOvlYVq0",
+	    authDomain: "shopping-backend-firebas-d3183.firebaseapp.com",
+	    databaseURL: "https://shopping-backend-firebas-d3183.firebaseio.com",
+	    projectId: "shopping-backend-firebas-d3183",
+	    storageBucket: "shopping-backend-firebas-d3183.appspot.com",
+	    messagingSenderId: "1075460438294",
+	    appId: "1:1075460438294:web:658ac6abeb7109de"
+    }
 };
 
 /*
